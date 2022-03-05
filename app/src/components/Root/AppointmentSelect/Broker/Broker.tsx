10eb41032b5486c83641f5bd6c59e6e48b056fbb
SirvanD <sirvansh@gmail.com>
2022-03-05 17:30:21 +1100

    completed 4 tasks

--- app/src/components/Root/AppointmentSelect/Broker/Broker.tsx
+++ app/src/components/Root/AppointmentSelect/Broker/Broker.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
 export interface BrokerProps {
   broker: {
@@ -6,17 +7,43 @@ export interface BrokerProps {
     id: number;
     appointments: { id: number; brokerId: number; date: string }[];
   };
+  setSelectedApt: React.Dispatch<React.SetStateAction<string>>;
 }
+const Li = styled.div`
+  background-color: #f3d62f;
+  font-size: 1.4rem;
+  margin: 1rem;
+  cursor: pointer;
+  display: inline-block;
+`;
+const Broker = ({ broker, setSelectedApt }: BrokerProps) => {
+  const [showAppointments, setShowAppointments] = useState(true);
 
-const Broker = (broker: BrokerProps) => {
+  const handleShowAppointments = () => {
+    setShowAppointments(!showAppointments);
+  };
+
   return (
     <li>
-      [broker name]
+      {broker.name}
       <br />
       appointments:
-      <button>Hide appointments</button>
+      <button onClick={handleShowAppointments}>Show/Hide appointments</button>
       <ul>
-        <li>[appointment date]</li>
+        <ul>
+          {showAppointments &&
+            broker.appointments.map((apt, id) => (
+              <Li
+                key={id}
+                value={apt.date}
+                onClick={(event) =>
+                  setSelectedApt(event.target.getAttribute("value"))
+                }
+              >
+                {apt.date}
+              </Li>
+            ))}
+        </ul>
       </ul>
     </li>
   );
