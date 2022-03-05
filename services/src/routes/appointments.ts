c62f306262536626196da4a51e4c8b402272953b
SirvanD <sirvansh@gmail.com>
2022-03-05 19:10:41 +1100

    completed Task 5

--- services/src/routes/appointments.ts
+++ services/src/routes/appointments.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 
-const appointments = [
+var appointments = [
   { id: 1, brokerId: 1, date: "15/10/2021" },
   { id: 2, brokerId: 3, date: "22/11/2021" },
   { id: 3, brokerId: 3, date: "23/11/2021" },
   { id: 4, brokerId: 4, date: "10/5/2021" },
 ];
+appointments.sort((a, b) => (a.date > b.date ? -1 : b.date > a.date ? 1 : 0));
+appointments.sort((a, b) =>
+  a.brokerId > b.brokerId ? 1 : b.brokerId > a.brokerId ? -1 : 0
+);
 
 const router = Router();
 
